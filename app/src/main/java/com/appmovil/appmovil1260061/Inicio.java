82b1441dfdc05ef9a8e7c57bd499efd6f8d1024f
aburgosg32 <aburgosg32@gmail.com>
2025-02-21 19:16:00 -0500

    Avance 21-02-25

--- app/src/main/java/com/appmovil/appmovil1260061/Inicio.java
+++ app/src/main/java/com/appmovil/appmovil1260061/Inicio.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,35 +11,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.button.MaterialButtonToggleGroup;
-
-public class Register extends AppCompatActivity {
+public class Inicio extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_inicio);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button btncancelar = findViewById(R.id.btnCancelarRegister);
-        Button btnregistrar = findViewById(R.id.btnRegistrarRegister);
-        Button btnlimpiar = findViewById(R.id.btnLimpiarRegister);
+        Button btningresar = findViewById(R.id.btnIngresarInicio);
 
-        EditText txtnombre = findViewById(R.id.txtApellidoNombre);
-        EditText txttelefono = findViewById(R.id.txtTelefonoRegister);
-        EditText txtemail = findViewById(R.id.txtCorreoRegister);
-        EditText txtpass = findViewById(R.id.txtPassRegister);
-
-        btncancelar.setOnClickListener(new View.OnClickListener() {
+        btningresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent conec = new Intent(Register.this,MainActivity.class);
-                startActivity(conec);
+                Intent conecinicio = new Intent(Inicio.this,MainActivity.class);
+                startActivity(conecinicio);
             }
         });
     }
